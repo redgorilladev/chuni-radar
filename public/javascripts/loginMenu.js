1f1575dacd51f967b222955721ed004d172e9a63
josh <woodabmxhd@gmail.com>
2024-10-08 22:29:15 +1100

    added sidebar menu and various css/layout changes

--- public/javascripts/loginMenu.js
+++ public/javascripts/loginMenu.js
@@ -1,18 +1,46 @@
 const loginDiv = document.getElementById("login-div");
 const adminLoginBtn = document.getElementById("openLoginForm");
+const closeMenuBtn = document.getElementById("closeMenu");
+const contentBlur = document.getElementById("content-blur");
+
+function lockScroll() {
+  // Calculate the scrollbar width
+  const scrollBarWidth =
+    window.innerWidth - document.documentElement.clientWidth;
+
+  // Apply the padding equal to the scrollbar width and hide the overflow
+  document.body.style.paddingRight = `${scrollBarWidth}px`;
+  document.body.style.overflow = "hidden";
+}
+
+function unlockScroll() {
+  // Remove the padding and allow scrolling again
+  document.body.style.paddingRight = "0px";
+  document.body.style.overflow = "";
+}
 
 if (adminLoginBtn != null) {
   adminLoginBtn.addEventListener("click", () => {
     if (loginDiv.classList.contains("closed")) {
-      loginDiv.style.transform = "translate(105%)";
+      loginDiv.style.transform = "translate(100%)";
       loginDiv.style.transition = "200ms ease-in-out";
-      loginDiv.classList.add("open");
-      loginDiv.classList.remove("closed");
-    } else {
-      loginDiv.style.transform = "translate(-100%)";
-      loginDiv.style.transition = "200ms ease-in-out";
-      loginDiv.classList.add("closed");
-      loginDiv.classList.remove("open");
+      loginDiv.classList.toggle("open");
+      loginDiv.classList.toggle("closed");
+      contentBlur.classList.toggle("blur");
+      contentBlur.style.opacity = "0.5";
+      lockScroll();
     }
+  });
+}
+
+if (closeMenuBtn != null) {
+  closeMenuBtn.addEventListener("click", () => {
+    loginDiv.style.transform = "translate(-100%)";
+    loginDiv.style.transition = "200ms ease-in-out";
+    loginDiv.classList.toggle("closed");
+    loginDiv.classList.toggle("open");
+    contentBlur.classList.toggle("blur");
+    contentBlur.style.opacity = "0";
+    unlockScroll();
   });
 }
